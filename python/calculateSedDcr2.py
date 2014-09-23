4f73cc63ac9acf2f1ed3ab6e8bcbc76d1f324489
Andy Becker <acbecker@gmail.com>
2014-09-23 15:04:50 -0500

    Fixing axis lables on calculateSedDcr2

--- python/calculateSedDcr2.py
+++ python/calculateSedDcr2.py
@@ -4,7 +4,8 @@ import lsst.sims.catalogs.measures.photometry.Sed as Sed
 import lsst.sims.catalogs.measures.photometry.Bandpass as Bandpass
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter, MultipleLocator
-majorLocatorx   = MultipleLocator(3)
+import cPickle
+majorLocatorx   = MultipleLocator(2)
 majorLocatory   = MultipleLocator(2)
 
 # This one aggregates on SED, and the calculates DCR w.r.t. km15_5250.fits_g05_5470
@@ -123,83 +124,89 @@ def integrate(sed, bp):
     return np.sum(fnu * bp.phi)
 
 if __name__ == "__main__":
-
-    catDir = os.environ["CAT_SHARE_DATA"]
-    filtDir = os.environ["LSST_THROUGHPUTS_BASELINE"]
-    gBand  = Bandpass()
-    rBand  = Bandpass()
-    iBand  = Bandpass()
-    zBand  = Bandpass()
-    gBand.readThroughput(os.path.join(filtDir, "total_g.dat"))
-    rBand.readThroughput(os.path.join(filtDir, "total_r.dat"))
-    iBand.readThroughput(os.path.join(filtDir, "total_i.dat"))
-    zBand.readThroughput(os.path.join(filtDir, "total_z.dat"))
-    gBand.sbTophi()
-    rBand.sbTophi()
-    iBand.sbTophi()
-    zBand.sbTophi()
-
-    ref = Sed()
-    ref.readSED_flambda(os.path.join(catDir, "SEDs/starSED/kurucz/%s"%(sedName)))
-    ref.flambdaTofnu()
-    wavelen        = ref.wavelen
-    fnu            = ref.fnu
-    waveleng, fnug = ref.resampleSED(wavelen, fnu, wavelen_match=gBand.wavelen)
-    wavelenr, fnur = ref.resampleSED(wavelen, fnu, wavelen_match=rBand.wavelen)
-    waveleni, fnui = ref.resampleSED(wavelen, fnu, wavelen_match=iBand.wavelen)
-    wavelenz, fnuz = ref.resampleSED(wavelen, fnu, wavelen_match=zBand.wavelen)
-    fluxg = fnug * gBand.phi
-    fluxr = fnur * rBand.phi
-    fluxi = fnui * iBand.phi
-    fluxz = fnuz * zBand.phi
-
-    refOffset = {}
-    refOffset["r"] = {}
-    refOffset["g"] = {}
-    refOffset["i"] = {}
-    refOffset["z"] = {}
-    for ia, dairmass in enumerate(dairmasses):
-        zd = np.arccos(1./(airmass1 + dairmass)) * 180/np.pi
-        refOffset["g"][int(zd)] = getOffset(waveleng, fluxg, zd) 
-        refOffset["r"][int(zd)] = getOffset(wavelenr, fluxr, zd) 
-        refOffset["i"][int(zd)] = getOffset(waveleni, fluxi, zd) 
-        refOffset["z"][int(zd)] = getOffset(wavelenz, fluxz, zd) 
-
-    allg = np.zeros((len(dairmasses), len(dthetas)))
-    allr = np.zeros((len(dairmasses), len(dthetas)))
-    alli = np.zeros((len(dairmasses), len(dthetas)))
-    allz = np.zeros((len(dairmasses), len(dthetas)))
-    args = []
-    seen = {}
-    for line in open(sys.argv[1]).readlines()[1:]:
-        sed, mag, nsed = line.split()
-
-        # TEST: remove the M-dwarfs
-        #if sed.startswith("m"):
-        #    continue
-
-        # OK, so I really want the mag in the respective passbands, so
-        # don't reject here
-        #mag = float(mag) 
-        #if mag < 16 or mag > 25:
-        #    continue
-
-        # Also, dont group by SED then, just do it
-        #if not sed in seen.keys():
-        #    seen[sed]  = 0
-        #seen[sed] += int(nsed)
-
-    #for sed,nsed in seen.items():
-    #    args.append((sed, mag, nsed))
-    #    ntot += nsed
+    pickleFile = "calculateSedDcr2M.pickle"
+    if not os.path.isfile(pickleFile):
+        catDir = os.environ["CAT_SHARE_DATA"]
+        filtDir = os.environ["LSST_THROUGHPUTS_BASELINE"]
+        gBand  = Bandpass()
+        rBand  = Bandpass()
+        iBand  = Bandpass()
+        zBand  = Bandpass()
+        gBand.readThroughput(os.path.join(filtDir, "total_g.dat"))
+        rBand.readThroughput(os.path.join(filtDir, "total_r.dat"))
+        iBand.readThroughput(os.path.join(filtDir, "total_i.dat"))
+        zBand.readThroughput(os.path.join(filtDir, "total_z.dat"))
+        gBand.sbTophi()
+        rBand.sbTophi()
+        iBand.sbTophi()
+        zBand.sbTophi()
+    
+        ref = Sed()
+        ref.readSED_flambda(os.path.join(catDir, "SEDs/starSED/kurucz/%s"%(sedName)))
+        ref.flambdaTofnu()
+        wavelen        = ref.wavelen
+        fnu            = ref.fnu
+        waveleng, fnug = ref.resampleSED(wavelen, fnu, wavelen_match=gBand.wavelen)
+        wavelenr, fnur = ref.resampleSED(wavelen, fnu, wavelen_match=rBand.wavelen)
+        waveleni, fnui = ref.resampleSED(wavelen, fnu, wavelen_match=iBand.wavelen)
+        wavelenz, fnuz = ref.resampleSED(wavelen, fnu, wavelen_match=zBand.wavelen)
+        fluxg = fnug * gBand.phi
+        fluxr = fnur * rBand.phi
+        fluxi = fnui * iBand.phi
+        fluxz = fnuz * zBand.phi
+    
+        refOffset = {}
+        refOffset["r"] = {}
+        refOffset["g"] = {}
+        refOffset["i"] = {}
+        refOffset["z"] = {}
+        for ia, dairmass in enumerate(dairmasses):
+            zd = np.arccos(1./(airmass1 + dairmass)) * 180/np.pi
+            refOffset["g"][int(zd)] = getOffset(waveleng, fluxg, zd) 
+            refOffset["r"][int(zd)] = getOffset(wavelenr, fluxr, zd) 
+            refOffset["i"][int(zd)] = getOffset(waveleni, fluxi, zd) 
+            refOffset["z"][int(zd)] = getOffset(wavelenz, fluxz, zd) 
+    
+        allg = np.zeros((len(dairmasses), len(dthetas)))
+        allr = np.zeros((len(dairmasses), len(dthetas)))
+        alli = np.zeros((len(dairmasses), len(dthetas)))
+        allz = np.zeros((len(dairmasses), len(dthetas)))
+        args = []
+        seen = {}
+        for line in open(sys.argv[1]).readlines()[1:]:
+            sed, mag, nsed = line.split()
+    
+            # TEST: remove the M-dwarfs
+            #if sed.startswith("m"):
+            #    continue
+    
+            # OK, so I really want the mag in the respective passbands, so
+            # don't reject here
+            #mag = float(mag) 
+            #if mag < 16 or mag > 25:
+            #    continue
+    
+            # Also, dont group by SED then, just do it
+            #if not sed in seen.keys():
+            #    seen[sed]  = 0
+            #seen[sed] += int(nsed)
+    
+        #for sed,nsed in seen.items():
+        #    args.append((sed, mag, nsed))
+        #    ntot += nsed
+            
+            mag = float(mag)
+            nsed = int(nsed)
+            args.append((sed, mag, nsed))
+    
+        import multiprocessing
+        pool = multiprocessing.Pool(multiprocessing.cpu_count()-2)
+        results = pool.map(doit, args)
+        #cPickle.dump(results, open(pickleFile, "wb"))
+    else:
+        results = cPickle.load(open(pickleFile, "rb"))
         
-        mag = float(mag)
-        nsed = int(nsed)
-        args.append((sed, mag, nsed))
 
-    import multiprocessing
-    pool = multiprocessing.Pool(multiprocessing.cpu_count()-2)
-    results = pool.map(doit, args)
     ntotg = ntotr = ntoti = ntotz = 0
     for i, result in enumerate(results):
         Mg, Mr, Mi, Mz, ng, nr, ni, nz = result
@@ -212,15 +219,24 @@ if __name__ == "__main__":
         ntoti += ni
         ntotz += nz
         print args[i][0], args[i][1], nr, nr, ni, nz
-    #import pdb; pdb.set_trace()
+    import pdb; pdb.set_trace()
+
+    # Actually its 1 bin too big
+    allg = allg[:-1,:-1]
+    allr = allr[:-1,:-1]
+    alli = alli[:-1,:-1]
+    allz = allz[:-1,:-1]
 
     fig, ((sp1, sp2),(sp3, sp4)) = plt.subplots(2, 2)
     fig.suptitle("Fraction of stars showing DCR offsets of %.3f arcsec (contour at %.2f)\n w.r.t. %s" % (dcrLevel, contLevel, sedName), weight="bold", fontsize=14)
+    fig.subplots_adjust(wspace=0.075)
 
     # Contour at 1% of stars show dipoles
+    xlabs = list(dthetas[::2])
+    xlabs.insert(0,0)
     hmap1 = sp1.pcolor(np.log10(allg/ntotg), cmap=plt.cm.Greys, shading="faceted", vmin=-2, vmax=0)
     sp1.contour(np.log10(allg/ntotg), levels=(np.log10(contLevel),), colors=("r"), linewidths=(3,), linestyles=("solid",))
-    sp1.set_xticklabels(dthetas[::2], minor=False, weight="bold", fontsize=12)
+    sp1.set_xticklabels(xlabs, minor=False, weight="bold", fontsize=12)
     sp1.set_yticklabels(airmass1+dairmasses[::2], minor=False, weight="bold", fontsize=12)
     sp1.set_title("g-band (%.2e total)"%(ntotg), weight="bold", fontsize=14)
     sp1.xaxis.set_major_locator(majorLocatorx)
@@ -228,16 +244,16 @@ if __name__ == "__main__":
 
     hmap2 = sp2.pcolor(np.log10(allr/ntotr), cmap=plt.cm.Greys, shading="faceted", vmin=-2, vmax=0)
     sp2.contour(np.log10(allr/ntotr), levels=(np.log10(contLevel),), colors=("r"), linewidths=(3,), linestyles=("solid",))
-    sp2.set_xticklabels(dthetas[::2], minor=False, weight="bold", fontsize=12)
+    sp2.set_xticklabels(xlabs, minor=False, weight="bold", fontsize=12)
     sp2.set_yticklabels(airmass1+dairmasses[::2], minor=False, weight="bold", fontsize=12)
-    cb2 = plt.colorbar(hmap2, ax=sp2, fraction=0.1)
+    cb2 = plt.colorbar(hmap2, ax=sp2, fraction=0.05, pad=0.025)
     sp2.set_title("r-band (%.2e total)"%(ntotr), weight="bold", fontsize=14)
     sp2.xaxis.set_major_locator(majorLocatorx)
     sp2.yaxis.set_major_locator(majorLocatory)
 
     hmap3 = sp3.pcolor(np.log10(alli/ntoti), cmap=plt.cm.Greys, shading="faceted", vmin=-2, vmax=0)
     sp3.contour(np.log10(alli/ntoti), levels=(np.log10(contLevel),), colors=("r"), linewidths=(3,), linestyles=("solid",))
-    sp3.set_xticklabels(dthetas[::2], minor=False, weight="bold", fontsize=12)
+    sp3.set_xticklabels(xlabs, minor=False, weight="bold", fontsize=12)
     sp3.set_yticklabels(airmass1+dairmasses[::2], minor=False, weight="bold", fontsize=12)
     sp3.set_title("i-band (%.2e total)"%(ntoti), weight="bold", fontsize=14)
     sp3.xaxis.set_major_locator(majorLocatorx)
@@ -245,9 +261,9 @@ if __name__ == "__main__":
 
     hmap4 = sp4.pcolor(np.log10(allz/ntotz), cmap=plt.cm.Greys, shading="faceted", vmin=-2, vmax=0)
     sp4.contour(np.log10(allz/ntotz), levels=(np.log10(contLevel),), colors=("r"), linewidths=(3,), linestyles=("solid",))
-    sp4.set_xticklabels(dthetas[::2], minor=False, weight="bold", fontsize=12)
+    sp4.set_xticklabels(xlabs, minor=False, weight="bold", fontsize=12)
     sp4.set_yticklabels(airmass1+dairmasses[::2], minor=False, weight="bold", fontsize=12)
-    cb4 = plt.colorbar(hmap4, ax=sp4, fraction=0.1)
+    cb4 = plt.colorbar(hmap4, ax=sp4, fraction=0.05, pad=0.025)
     sp4.set_title("z-band (%.2e total)"%(ntotz), weight="bold", fontsize=14)
     sp4.xaxis.set_major_locator(majorLocatorx)
     sp4.yaxis.set_major_locator(majorLocatory)
